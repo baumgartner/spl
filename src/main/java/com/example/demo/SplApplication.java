d92ce97cfbb670b67005d1c054c38b6779bcc3a0
Martin Baumgartner <martin.baumgartner26@gmail.com>
2018-03-21 21:43:16 +0100

    redirect after login

--- src/main/java/com/example/demo/SplApplication.java
+++ src/main/java/com/example/demo/SplApplication.java
@@ -2,11 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.demo.dao.Box;
-import com.example.demo.dao.BoxStatus;
-import com.example.demo.dao.Location;
 
 @SpringBootApplication
 public class SplApplication {
