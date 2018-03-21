d635125352f2c18e1a311f7bbedb2b4723e8bfbc
Florian Walter <florian@fwalter.at>
2018-03-21 17:23:26 +0100

    h

--- src/main/java/com/example/demo/dao/BookEntry.java
+++ src/main/java/com/example/demo/dao/BookEntry.java
@@ -6,9 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import org.hibernate.annotations.Type;
-import org.joda.time.DateTime;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
