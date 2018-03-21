b7685347e57ae2de0c18fa0499e449ebc0cef896
Florian Walter <florian@fwalter.at>
2018-03-21 14:51:01 +0100

    cleanup

--- src/main/java/com/example/demo/web/OverviewController.java
+++ src/main/java/com/example/demo/web/OverviewController.java
@@ -35,14 +35,7 @@ public class OverviewController {
 	
 	@Autowired
 	private BookEntryRepository bookEntryRepository;
-	
-//	@GetMapping("/greeting")
-//	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-//			Model model) {
-//		model.addAttribute("name", name);
-//		return "greeting";
-//	}
-	
+		
 	@PostMapping("/login")
 	public String login(@RequestParam(name = "username", required = true) String username, 
 			@RequestParam(name = "password", required = true) String password,
